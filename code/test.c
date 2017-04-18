a8a9933a4acbc7222c53e9857c097a23f0b1d520
sammanthp007 <samman.thapa@bison.howard.edu>
2017-04-18 15:23:00 -0400

    comment

--- code/test.c
+++ code/test.c
@@ -28,23 +28,75 @@ rtems_task Init(rtems_task_argument argument);
 const char rtems_test_name[] = "Wait queue and Timer Manager";
 rtems_printer rtems_test_printer;
 
-rtems_task Init(
-  rtems_task_argument ignored
-)
-{
-  rtems_print_printer_printf(&rtems_test_printer);
-  rtems_test_begin();
-  
-  /* edit start 
-  rtems_object_name result;
-  result = rtems_build_name('S', 'A', 'M', 'M');
-  printf( "ID= name=%s\n", ((result) ? result : "no name") );
-*/
-  printf( "Write two tasks that use Timer Manager to schedule a function that should execute every 0.25 seconds. \n" );
+/* TODO: define global variable counter */
 
-  /* edit ends */
-  rtems_test_end();
-  exit( 0 );
+
+/* TODO: define global variable reset_count */
+
+
+/* TODO
+ * Create a function wakeup_and_edit_counter
+ * Reset global Counter every 0.25 seconds
+ * and increase reset_count every 15 seconds
+ */
+
+
+/* TODO
+ * Create a function wakeup_and_print that
+ * Print out the value of counter
+ * and print out the value of reset_count
+ * then make the task 2 to sleep
+ */
+
+
+rtems_task Init(
+        rtems_task_argument ignored
+        )
+{
+    rtems_print_printer_printf(&rtems_test_printer);
+    rtems_test_begin();
+
+    /* edit start */
+    printf("Write two tasks that use Timer Manager to schedule a function that should execute every 0.25 seconds. \n" );
+
+    /* TODO
+     * Create first task
+     * Calls wakeup_and_edit_counter
+     * */
+
+    /* TODO
+     * Create Second task
+     * calls wakeup_and_print
+     * */
+
+
+    /* TODO
+     * Create Timer Manager using rtems_timer_create directive
+     * Set interval of 0.25 sec using rtems_timer_fire_after
+     */
+    rtems_status_code rtems_timer_create(
+            rtems_name name,
+            rtems_id *id
+            );
+
+    rtems_status_code rtems_timer_fire_after(
+            rtems_id id,
+            rtems_interval ticks, 
+            rtems_timer_service_routine_entry  routine,
+            void *user_data 
+            );
+
+
+    /* TODO
+     * Create the RMS manager using rtems_rate_monotonic_create for the function
+     * and allocate period of 15 sec using rtems_rate_monotonic_period
+     */
+
+
+
+    /* edit ends */
+    rtems_test_end();
+    exit( 0 );
 }
 
 
